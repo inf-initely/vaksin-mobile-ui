249789276333c9e1c772c62ae16cd01d6e626266
inf-initely <aimanfauzy.99@gmail.com>
2021-11-24 17:19:19 +0800

    Updates

--- dist/App.js
+++ dist/App.js
@@ -1,7 +1,6 @@
 import {ChakraProvider} from "../_snowpack/pkg/@chakra-ui/react.js";
 import React from "../_snowpack/pkg/react.js";
 import {Outlet} from "../_snowpack/pkg/react-router.js";
-import "./App.css.proxy.js";
 import Theme from "./themes/index.js";
 function App({}) {
   return /* @__PURE__ */ React.createElement(ChakraProvider, {
