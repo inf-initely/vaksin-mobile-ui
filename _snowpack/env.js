05faebfe25fd297a5e79e331a9c613e78d880aac
inf-initely <aimanfauzy.99@gmail.com>
2021-12-08 13:58:29 +0800

    Updates

--- _snowpack/env.js
+++ _snowpack/env.js
@@ -1,4 +1,4 @@
-export const SNOWPACK_PUBLIC_API_URL = "/vaksin-mobile-ui";
+export const SNOWPACK_PUBLIC_API_URL = "/vaksin-mobile-ui/";
 export const MODE = "production";
 export const NODE_ENV = "production";
 export const SSR = false;
